295e0a739d52dc4a030461937e1609b8312ab822
dinesh <arekardinesh685@gmail.com>
2024-02-06 02:48:26 +0530

    same changes

--- controller.js
+++ controller.js
@@ -1,36 +1,48 @@
-const shortid=require('shortid')
-const URL=require('./model')
+const shortid = require('shortid')
+const URL = require('./model')
 
-async function generatenewurl(req,res){
-    const body=req.body;
+async function generatenewurl(req, res) {
+    const body = req.body;
     if (!body || !body.url) {
         return res.status(400).json({ error: 'url is required' });
     }
-    const newShortId=shortid();
-    
+    const newShortId = shortid();
+
     await URL.create({
-        shorturlid:newShortId,
-        redirecturl:body.url,
-        visithistory:[]
+        shorturlid: newShortId,
+        redirecturl: body.url,
+        visithistory: []
     })
-    return res.json({id:newShortId})
+    return res.json({ id: newShortId })
 }
-async function funredirecturl(req,res){
-    const shorturlid=req.params.shorturlid;
-    const entry= await URL.findOneAndUpdate({
+async function funredirecturl(req, res) {
+    const shorturlid = req.params.shorturlid;
+    const entry = await URL.findOneAndUpdate({
         shorturlid
-    },{$push:{
-        visithistory:{
-            timestamp:Date.now()
+    }, {
+        $push: {
+            visithistory: {
+                timestamp: Date.now()
+            }
         }
-    }})
+    })
 
     res.redirect(entry.redirecturl)
 }
 
+async function analitics(res, req) {
+    const shorturlid = req.params.shorturlid;
+    const result = await URL.findOne({ shorturlid })
+    return res.json({
+        clicks: result.visithistory.length,
+        Analitics: result.visithistory
+    })
+}
 
 
-module.exports={
+
+module.exports = {
     generatenewurl,
-    funredirecturl
+    funredirecturl,
+    analitics
 }
--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const express=require('express');
-const {generatenewurl,funredirecturl}=require('./controller')
+const {generatenewurl,funredirecturl,analitics}=require('./controller')
 const router=express.Router();
 
 router.post('/',generatenewurl)
 
 router.get('/:shorturlid',funredirecturl)
+
+router.get('/analitics/:shorturlid',analitics)
 
 module.exports=router;
